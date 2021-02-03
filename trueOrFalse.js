42f7a2a83820b4fd18a4fd12d9fed72a91ea7004
tania chanda <apuchandallb222@gmail.com>
2021-02-03 17:29:28 +0600

    True Or False

---
+++ trueOrFalse.js
@@ -0,0 +1,19 @@
+// TRUTH:
+// '0', ' ', []
+
+// FALSE:
+// false
+// 0
+// ""
+// undefined
+// null
+// NaN
+
+
+let name = '';
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
