b9f061ca239253b975eb1b6b52f103b45f6674e8
tania chanda <apuchandallb222@gmail.com>
2021-02-03 17:57:14 +0600

    undefined vs nul

---
+++ undefinedVsNull.js
@@ -0,0 +1,17 @@
+let Jin;
+function add(num1, num2){
+    console.log(num1+num2);
+    return
+}
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+const bts = {name:"Kim Seok-jin", phone:1234567};
+console.log(bts.txt);
+
+let fan = undefined;
+console.log(fan);
+
+let army = [2, 3, 7]
+console.log(army[22]);
